ed1e3e0517fa3cf70de6538b67c24e80eb4c9496
Santosh Dangal <dangalsantosh9@gmail.com>
2026-02-26 23:13:06 -0600

    Fix PORT for Render

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-import 'dotenv/config'; // Load environment variables
+// src/index.ts
+import 'dotenv/config';
+import express, { Express } from 'express';
+
+import { createPet, deletePet, getPet, listPets, updatePetName } from './controllers/pets.js';
+
+import { createHabit, listHabits } from './controllers/habits.js';
+import { createLog } from './controllers/logs.js';
+
+const app: Express = express();
+
+app.use(express.json());
+
+// --- Routes ---
+app.post('/pets', createPet);
+app.get('/pets', listPets);
+app.get('/pets/:petId', getPet);
+app.put('/pets/:petId', updatePetName);
+app.delete('/pets/:petId', deletePet);
+
+app.post('/pets/:petId/habits', createHabit);
+app.get('/pets/:petId/habits', listHabits);
+
+app.post('/pets/:petId/logs', createLog);
+// --- Routes ---
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Tender listening on http://localhost:${PORT}`);
+});
